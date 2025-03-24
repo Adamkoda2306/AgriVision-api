c41927b25dda0047191acbf16aafc87cc94709fd
Adamkoda2306 <147936412+Adamkoda2306@users.noreply.github.com>
2025-03-24 21:28:56 +0530

    Removed sensitive files

--- app.py
+++ app.py
@@ -270,6 +270,4 @@ def translate_description():
         return jsonify({"error": str(e)}), 500
 
 if __name__ == '__main__':
-    logging.info("Starting Flask server on http://127.0.0.1:5000")
-    port = int(os.environ.get("PORT", 5000))
-    app.run(host="0.0.0.0", port=port)
+    app.run(host="0.0.0.0", port=5000)
