e7febce9b99f8eeb9e328405762089ac272410f6
AdamKoda2306 <adam.koda2306@gmail.com>
2026-01-25 23:29:42 +0530

    authentication routes

--- routes/auth.routes.ts
+++ routes/auth.routes.ts
@@ -3,12 +3,16 @@ import { login, resendOTP, verify } from '../services/auth.service';
 
 const router = express.Router();
 
+
+// ----------------- LOGIN ROUTE ----------------------------
 router.post('/login', async (req: Request, res: Response) => {
     const phonenumber: string = req.body.phonenumber;
     const result: { statusCode: number, success: boolean, message: string } = await login(phonenumber);
     return res.status(result.statusCode).json({ success: result.success, message: result.message });
 });
 
+
+// ----------------- VERIFY ROUTE ----------------------------
 router.post('/verify', async (req: Request, res: Response) => {
     const phonenumber: string = req.body.phonenumber;
     const otp: string = req.body.otp;
@@ -16,6 +20,8 @@ router.post('/verify', async (req: Request, res: Response) => {
     return res.status(result.statusCode).json({ success: result.success, message: result.message, ...(result.token && {token: result.token})});
 });
 
+
+// ----------------- RESEND OTP ROUTE ----------------------------
 router.post('/resend-otp', async (req: Request, res: Response) => {
     const phonenumber: string = req.body.phonenumber;
     const result: { statusCode: number, success: boolean, message: string } = await resendOTP(phonenumber);
