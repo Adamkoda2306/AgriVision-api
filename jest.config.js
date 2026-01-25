1fee15eeecf0341a269528728c7890ac9c866388
AdamKoda2306 <adam.koda2306@gmail.com>
2026-01-25 23:56:49 +0530

    Fixed Package issues conflicts

--- jest.config.js
+++ jest.config.js
@@ -4,10 +4,14 @@ const tsJestTransformCfg = createDefaultPreset().transform;
 
 /** @type {import("jest").Config} **/
 module.exports = {
+  preset: "ts-jest",
   testEnvironment: "node",
   testTimeout: 20000,
   setupFilesAfterEnv: ["./test/setup.ts"],
   transform: {
     ...tsJestTransformCfg,
   },
+  transformIgnorePatterns: [
+    "node_modules/(?!(uuid)/)"
+  ],
 };
